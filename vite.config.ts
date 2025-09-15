ece9038534a7b0e6c2e12470d18d8b6fafa5a446
raffff14 <rafhaelmalabanan1@gmail.com>
2025-09-16 00:52:25 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/porfolioraf/',  // <-- must match your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
